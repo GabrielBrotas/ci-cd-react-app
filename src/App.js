f5afa867d395709cfba6dd535af78bc46a94192a
GabrielBrotas <gabrielbrotas22@gmail.com>
2022-03-08 22:22:35 -0300

    update version again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v1.3
+          Learn React v1.5
         </a>
       </header>
     </div>
