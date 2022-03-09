694123c7269054b979a10bb3ac5d97d1a25fc509
GabrielBrotas <gabrielbrotas22@gmail.com>
2022-03-08 22:25:02 -0300

    fix test script

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v1.5
+          Learn React v1.6
         </a>
       </header>
     </div>
